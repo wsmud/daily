e82b2b7f3d191731486f5872fd681f4e8e25d0c8
wsmud <thisline@163.com>
2022-03-15 14:53:43 +0800

    fix: 修复items数组存在0的情况

--- source/events/sect/items.js
+++ source/events/sect/items.js
@@ -4,7 +4,7 @@ module.exports = function (data) {
   }
 
   for (const item of data.items) {
-    if (item.p || !item.name) {
+    if (!item || item.p || !item.name) {
       continue;
     }
 
--- source/events/tower/items.js
+++ source/events/tower/items.js
@@ -2,7 +2,7 @@ const timers = require('../../utils/timers');
 
 module.exports = function (data) {
   for (const item of data.items) {
-    if (item.p || !item.name) {
+    if (!item || item.p || !item.name) {
       continue;
     }
 
