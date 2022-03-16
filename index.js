09698f9a1a66ac7f90f033e00a1bf6988b4c00ea
wsmud <thisline@163.com>
2022-03-16 11:38:22 +0800

    feat: 添加立即运行一次指令

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const yaml = require('js-yaml');
+const { Command } = require('commander');
 const schedule = require('node-schedule');
 const Daily = require('./source/librarys/daily');
+const program = new Command();
 
 function loginQueue(configs, userConfig) {
   const user = new Daily(userConfig);
@@ -19,3 +21,16 @@ schedule.scheduleJob('5 5 5 * * *', () => {
     loginQueue(configs, userConfig);
   });
 });
+
+program.option('-r, --run', '立即运行一次');
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+if (options.run) {
+  const configs = yaml.load(fs.readFileSync('config.yaml'));
+  configs.splice(0, 30).forEach((userConfig) => {
+    loginQueue(configs, userConfig);
+  });
+}
