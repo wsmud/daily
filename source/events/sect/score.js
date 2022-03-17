ab9473c42253beed4cb8557bc2344f0234b62eed
wsmud <thisline@163.com>
2022-03-17 11:02:56 +0800

    fix: 修复引用问题

--- source/events/sect/score.js
+++ source/events/sect/score.js
@@ -6,7 +6,7 @@ module.exports = function (data) {
     logger.error(`「${this.userConfig.name}」无法确认用户门派，用户门派为「${data.family}}」`);
     return this.socketClose();
   }
-  this.sect = userSect;
+  this.sect = JSON.parse(JSON.stringify(userSect));
   this.userLevel = data.level;
   this.cmd.send(`stopstate;${this.sect.taskerWay}`);
 };
