99eaa5407afbdfc9bbb33ad460a4f6255f7fd71b
wsmud <thisline@163.com>
2022-03-17 14:27:25 +0800

    refactor: 重构cli

--- source/utils/cli.js
+++ source/utils/cli.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const yaml = require('js-yaml');
 const chalk = require('chalk');
-const prompts = require('prompts');
+const inquirer = require('inquirer');
 const getServer = require('./getServer');
 const getToken = require('./getToken');
 const Socket = require('../librarys/socket');
@@ -46,166 +46,151 @@ const dungeons = {
 };
 
 (async () => {
-  let answers = {};
-
-  await prompts(
-    [
-      {
-        type: () =>
-          fs.existsSync('config.yaml') && fs.statSync('config.yaml').isFile() ? 'confirm' : null,
-        name: 'add',
-        message: '检测到配置文件，是否向原配置文件新增',
-        initial: true,
-      },
-      {
-        type: 'select',
-        name: 'server',
-        message: '服务器？服务器？服务器？',
-        choices: [
-          { title: '一区', value: 0 },
-          { title: '二区', value: 1 },
-          { title: '三区', value: 2 },
-          { title: '四区', value: 3 },
-          { title: '测试服', value: 4 },
-        ],
-        hint: '上下键移动，空格或回车键选择',
-        async format(prev) {
-          return await getServer(prev);
-        },
-      },
-      {
-        type: 'text',
-        name: 'account',
-        message: '账号？账号？账号？',
-        validate(prev) {
-          return prev.length < 1 ? '输账号啊！丢雷楼某！' : true;
-        },
-      },
-      {
-        type: 'password',
-        name: 'password',
-        message: '密码？密码？密码？',
-        async validate(prev) {
-          if (prev.length < 1) {
-            return '输密码啊！丢雷楼某！';
-          }
-
-          return (await getToken(answers.account, prev)) ? true : '密码输入错误';
-        },
-        async format(prev) {
-          return await getToken(answers.account, prev);
-        },
-      },
-      {
-        type: 'multiselect',
-        name: 'roles',
-        message: '哪些角色？',
-        hint: '上下键移动，空格键选中，A键全选',
-        instructions: false,
-        async choices() {
-          return await new Promise((resolve) => {
-            const socket = new Socket({
-              server: answers.server,
-              token: answers.password,
-            });
-
-            socket.on('roles', (data) => {
-              socket.socketClose();
-
-              if (data.roles.length < 1) {
-                console.log(chalk.red('淦！一个角色都没有做什么日常！'));
-                process.exit();
-              }
-
-              resolve(
-                data.roles.map(({ id, name }) => ({
-                  value: { id, name },
-                  title: name,
-                })),
-              );
-            });
-          });
-        },
-      },
-    ],
+  const rolesQuestions = [
     {
-      onSubmit(prpmpt, answer, answersSoFar) {
-        answers = { ...answers, ...answersSoFar };
+      type: 'confirm',
+      name: 'add',
+      message: '检测到配置文件，是否向原配置文件新增',
+      default: true,
+      when() {
+        return fs.existsSync('config.yaml') && fs.statSync('config.yaml').isFile();
       },
-      onCancel: () => process.exit(),
     },
-  );
-
-  let config = answers.roles.map(({ id, name }) => {
-    return {
-      name,
-      server: answers.server,
-      token: `${answers.password} ${id}`,
-    };
-  });
-
-  const dungeonQuestions = [];
-
-  config.forEach((user) => {
-    const dungeonQuestion = {
-      type: 'select',
-      name: user.name,
-      message: `${user.name}的副本？`,
-      choices() {
-        const selects = [];
-        for (const dungeonName in dungeons) {
-          selects.push({
-            title: dungeonName,
-            value: dungeonName,
-          });
-        }
-        return selects;
-      },
-    };
-    const difficultyQuestion = {
-      type: (prev) => (dungeons[prev].hard ? 'select' : null),
-      name: `${user.name}hard`,
-      message: `${user.name}的副本难度？`,
+    {
+      type: 'list',
+      name: 'server',
+      message: '服务器？',
       choices: [
-        {
-          title: '普通',
-          value: 0,
-        },
-        {
-          title: '困难',
-          value: 1,
-        },
+        { name: '一区', value: 0 },
+        { name: '二区', value: 1 },
+        { name: '三区', value: 2 },
+        { name: '四区', value: 3 },
+        { name: '测试服', value: 4 },
       ],
-    };
-    dungeonQuestions.push(dungeonQuestion, difficultyQuestion);
+      async filter(thisAnswer) {
+        return await getServer(thisAnswer);
+      },
+    },
+    {
+      type: 'input',
+      name: 'account',
+      message: '账号？',
+      validate(thisAnswer) {
+        return thisAnswer.length < 1 ? chalk.red('输账号啊！') : true;
+      },
+    },
+    {
+      type: 'password',
+      name: 'password',
+      mask: '*',
+      message: '密码？',
+      async validate(thisAnswer, answers) {
+        if (thisAnswer.length < 1) {
+          return chalk.red('输密码啊!');
+        }
+
+        const checkPassword = await getToken(answers.account, thisAnswer);
+        return !!checkPassword ? true : chalk.red('密码错误！');
+      },
+    },
+    {
+      type: 'checkbox',
+      name: 'roles',
+      message: '角色？',
+      async choices(answers) {
+        const token = await getToken(answers.account, answers.password);
+        return await new Promise((resolve) => {
+          const socket = new Socket({
+            server: answers.server,
+            token,
+          });
+
+          socket.on('roles', (data) => {
+            socket.socketClose();
+
+            if (data.roles.length < 1) {
+              console.log(chalk.red('淦！一个角色都没有做什么日常！'));
+              process.exit();
+            }
+
+            resolve(
+              data.roles.map(({ id, name }) => ({
+                name,
+                value: {
+                  name,
+                  server: answers.server,
+                  token: `${token} ${id}`,
+                },
+              })),
+            );
+          });
+        });
+      },
+      validate(thisAnswer) {
+        return thisAnswer.length < 1
+          ? chalk.red('不选角色做什么日常！')
+          : thisAnswer.length > 10
+          ? chalk.red('太多了！')
+          : true;
+      },
+    },
+  ];
+  const rolesAnswers = await inquirer.prompt(rolesQuestions);
+  const dungeonQuestions = [];
+  rolesAnswers.roles.forEach(({ name }) => {
+    dungeonQuestions.push(
+      {
+        type: 'list',
+        name,
+        message: `${name}的副本？`,
+        choices() {
+          return Object.keys(dungeons);
+        },
+      },
+      {
+        type: 'list',
+        name: `${name}_dungeonDifficulty`,
+        message: `${name}的副本难度？`,
+        choices: [
+          {
+            name: '普通',
+            value: 0,
+          },
+          {
+            name: '困难',
+            value: 1,
+          },
+        ],
+        when(answers) {
+          return dungeons[answers[name]].hard;
+        },
+      },
+    );
   });
 
-  const dungeonAnswers = await prompts(dungeonQuestions);
-
-  config = config.map((user) => {
-    const newUserConfig = {
-      ...user,
-      dungeon: `cr ${dungeons[dungeonAnswers[user.name]].id} ${
-        dungeonAnswers[`${user.name}hard`] ? 1 : 0
+  const dungeonAnswers = await inquirer.prompt(dungeonQuestions);
+  const fainlAnswers = rolesAnswers.roles.map((role) => {
+    return {
+      ...role,
+      dungeon: `cr ${dungeons[dungeonAnswers[role.name]].id} ${
+        dungeonAnswers[`${role.name}_dungeonDifficulty`] ? 1 : 0
       }`,
     };
-
-    return newUserConfig;
   });
 
-  if (fs.existsSync('config.yaml') && fs.statSync('config.yaml').isFile() && answers.add) {
-    let lastConfig = yaml.load(fs.readFileSync('config.yaml'));
-    !lastConfig && (lastConfig = []);
-    config.forEach((userConfig) => {
-      const oldConfigIndex = lastConfig.findIndex(({ name }) => name === userConfig.name);
+  if (fs.existsSync('config.yaml') && fs.statSync('config.yaml').isFile() && rolesAnswers.add) {
+    let oldConfig = yaml.load(fs.readFileSync('config.yaml'));
+    !oldConfig && (oldConfig = []);
+    fainlAnswers.forEach((userConfig) => {
+      const oldConfigIndex = oldConfig.findIndex(({ name }) => name === userConfig.name);
       if (oldConfigIndex >= 0) {
-        lastConfig.splice(oldConfigIndex, 1, userConfig);
+        oldConfig.splice(oldConfigIndex, 1, userConfig);
       } else {
-        lastConfig.push(userConfig);
+        oldConfig.push(userConfig);
       }
     });
-    fs.writeFileSync('config.yaml', yaml.dump(lastConfig));
+    fs.writeFileSync('config.yaml', yaml.dump(oldConfig));
   } else {
-    fs.writeFileSync('config.yaml', yaml.dump(config));
+    fs.writeFileSync('config.yaml', yaml.dump(fainlAnswers));
   }
 })();
