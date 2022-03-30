f3a334edd994362c56b6703ac7b1b0f0fcfde61d
wsmud <thisline@163.com>
2022-03-30 09:37:35 +0800

    fix: 修复武道塔可失败次数

--- source/events/tower/tip.js
+++ source/events/tower/tip.js
@@ -31,7 +31,7 @@ module.exports = function (tip) {
     clearInterval(this.timers.pfm);
     clearInterval(this.timers.up);
     this.combatFailedNum++;
-    if (this.combatFailedNum >= 1) {
+    if (this.combatFailedNum >= 3) {
       this.combatFailedNum = 0;
       setTimeout(() => {
         this.cmd.send(this.gameInfo.bank.way);
