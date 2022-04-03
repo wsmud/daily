f3be5043d61deb4b6a72f3d42d23a027d391827c
wsmud <thisline@163.com>
2022-04-03 11:47:57 +0800

    fix: 修复异常退出未正常闭关或挖矿的问题

--- source/events/sect/tip.js
+++ source/events/sect/tip.js
@@ -7,7 +7,9 @@ module.exports = function (tip) {
 
   if (tip.includes('你去帮我找')) {
     this.sectTaskInfo.taskItem = tip.match(/<.+?>.+?<\/.+?>/)[0];
-    const seller = this.gameInfo.store.find(({ goods }) => goods.includes(this.sectTaskInfo.taskItem));
+    const seller = this.gameInfo.store.find(({ goods }) =>
+      goods.includes(this.sectTaskInfo.taskItem),
+    );
     if (seller) {
       this.sectTaskInfo.seller = seller.seller;
       this.cmd.send(seller.way);
@@ -21,12 +23,27 @@ module.exports = function (tip) {
 
   if (tip.includes('你拿不下那么多东西')) {
     logger.warning(`「${this.userConfig.name}」背包已满`);
-    this.socketClose();
+    this.cmd.send(this.userConfig.logoutCommand);
+    this.cmd.send(
+      /ord|hio/.test(this.userLevel)
+        ? 'jh fam 0 start;go west;go west;go north;go enter;go west;xiulian'
+        : 'wakuang',
+    );
   }
 
   if (tip.includes('你没有那么多的钱')) {
     logger.warning(`「${this.userConfig.name}」黄金不足`);
+    this.cmd.send(this.userConfig.logoutCommand);
+    this.cmd.send(
+      /ord|hio/.test(this.userLevel)
+        ? 'jh fam 0 start;go west;go west;go north;go enter;go west;xiulian'
+        : 'wakuang',
+    );
+  }
+
+  if (/你挥着铁镐开始认真挖矿|你盘膝坐下开始闭关修炼/.test(tip)) {
     this.socketClose();
+    logger.success(`「${this.userConfig.name}」退出登录`);
   }
 
   if (/你先去休息一下吧/.test(tip)) {
